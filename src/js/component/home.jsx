3c7ae7d2fc72aea86a8b99ff6ac9347076bfc229
Khrise Franchesca <129009072+Khrisefzm@users.noreply.github.com>
2023-07-05 15:48:17 +0000

    form with addTasks function

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,57 @@
 import React, { useState, useEffect } from "react";
-/*import Form from "./form.jsx";
+import Form from "./form.jsx";
 import Task from "./task.jsx";
-import TaskNumber from "./taskNumber.jsx";*/
+//import TaskNumber from "./taskNumber.jsx";
 import Login from "./login.jsx";
 
 //create your first component
-
 const Home = () => {
+	const url = "https://assets.breatheco.de/apis/fake/todos/user/";
+	const [tasks, setTasks] = useState([{
+		label:"",
+		done:false
+	}]);
+	const [user, setUser] = useState("");
+
+	const addUser = (user) => {
+		setUser(user);
+	}
+	
+	const loadTask = (task) => { //task is an array with objects
+		setTasks(task);
+		console.log(tasks);
+	}
+
+	const addTask = (task) => {
+		let newTasks = [...tasks, task];
+		setTasks(newTasks);
+	}
+
+	fetch(`${url}${user}`,{
+		method: "PUT",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(tasks)
+	})
+	.then(response => {return response.JSON})
+	.then(response => console.log(response))
+	.catch(error=> console.log(error))
+
+	console.log(`the user is: ${user}`)
 	return (
 		<div className="container">
-			<Login />
+			<Login loadTask={loadTask} addUser={addUser} url={url}>
+				<Form addTask={addTask} url={url} user={user}/>
+				{user != "" ? 
+					tasks.map((element, key) => {
+						return (
+							<Task key={key} text={element.label} />
+						);
+					})
+					: <></>
+				}
+			</Login>
 		</div>	
 	)
 }
--- src/js/component/task.jsx
+++ src/js/component/task.jsx
@@ -1,5 +1,21 @@
 import React, {useState} from "react";
 
-
+function Task ({text}) {
+    
+    return(
+        <div className="row border-top border-bottom p-2 m-0" >
+            <div className="col">
+                <p className="m-0">{text}</p>
+            </div>
+            <div className="col-1">
+               <button 
+                    type="button" 
+                    className={`btn-close`} 
+                    aria-label="Close"
+                ></button> 
+            </div>
+        </div>
+    )
+}
 
 export default Task;
